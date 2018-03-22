c52776aaea6d7c7d0c0f86dd40016b3937d7fcf5
Ryan Rauch <rauch.ryan@gmail.com>
2018-03-22 08:54:20 -0500

    app icons

--- BerlinClockApp/BerlinClockApp.iOS/Properties/AssemblyInfo.cs
+++ BerlinClockApp/BerlinClockApp.iOS/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BerlinClockApp.iOS")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018 Ryan Rauch")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
